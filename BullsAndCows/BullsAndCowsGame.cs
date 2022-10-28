392e0bdd364ea59118d399fbf81efed848f4b7b7
Bingqi <BLiu25@slb.com>
2022-10-28 15:16:56 +0800

    feat: Should_return_2A0B_when_guess_given_the_guess_having_2_digits_with_value_and_position_are_same_as_secret(string secret, string guessDigits)

--- BullsAndCows/BullsAndCowsGame.cs
+++ BullsAndCows/BullsAndCowsGame.cs
@@ -5,16 +5,36 @@ namespace BullsAndCows
     public class BullsAndCowsGame
     {
         private readonly SecretGenerator secretGenerator;
+        private string secret;
         public BullsAndCowsGame(SecretGenerator secretGenerator)
         {
             this.secretGenerator = secretGenerator;
+            secret = secretGenerator.GenerateSecret();
         }
 
         public bool CanContinue => true;
 
         public string Guess(string guess)
         {
-            throw new NotImplementedException();
+            var countBulls = CountBulls(guess);
+
+            return $"{countBulls}A0B";
+        }
+
+        private int CountBulls(string guess)
+        {
+            var guessDigits = guess.Split(separator: " ");
+            var secretDigits = secret.Split(separator: " ");
+            int countBulls = 0;
+            for (var index = 0; index < 4; index++)
+            {
+                if (guessDigits[index] == secretDigits[index])
+                {
+                    countBulls++;
+                }
+            }
+
+            return countBulls;
         }
     }
 }
--- BullsAndCowsTest/BullsAndCowsGameTest.cs
+++ BullsAndCowsTest/BullsAndCowsGameTest.cs
@@ -1,4 +1,5 @@
 using BullsAndCows;
+using Moq;
 using Xunit;
 
 namespace BullsAndCowsTest
@@ -12,6 +13,35 @@ namespace BullsAndCowsTest
             var game = new BullsAndCowsGame(secretGenerator);
             Assert.NotNull(game);
             Assert.True(game.CanContinue);
+        }
+
+        [Fact]
+        public void Should_return_4A0B_when_guess_given_the_guess_digits_are_same_as_secret()
+        {
+            //given
+            var mockGenerator = new Mock<SecretGenerator>();
+            mockGenerator.Setup(generator => generator.GenerateSecret()).Returns("1 2 3 4");
+            var game = new BullsAndCowsGame(mockGenerator.Object);
+            //when
+            var guessResult = game.Guess("1 2 3 4");
+            //then
+            Assert.Equal("4A0B", guessResult);
+        }
+
+        [Theory]
+        [InlineData("1 2 3 4", "1 2 5 6")]
+        [InlineData("1 2 3 4", "5 2 3 6")]
+        [InlineData("1 2 3 4", "1 7 3 6")]
+        public void Should_return_2A0B_when_guess_given_the_guess_having_2_digits_with_value_and_position_are_same_as_secret(string secret, string guessDigits)
+        {
+            //given
+            var mockGenerator = new Mock<SecretGenerator>();
+            mockGenerator.Setup(generator => generator.GenerateSecret()).Returns(secret);
+            var game = new BullsAndCowsGame(mockGenerator.Object);
+            //when
+            var guessResult = game.Guess(guessDigits);
+            //then
+            Assert.Equal("2A0B", guessResult);
         }
     }
 }
